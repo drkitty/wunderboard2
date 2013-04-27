592916e1571534af420a1dde080a1b2613a89459
drkitty <bumblingkitty@gmail.com>
2013-04-26 18:06:51 -0700

    Remove incorrect "program" description

--- initialize.c
+++ initialize.c
@@ -23,11 +23,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  *
- * @section DESCRIPTION
- *
- * This program will test the various components of the Wunderboard when used
- * with the corresponding host test script.
- *
  */
 
 #include "initialize.h"
